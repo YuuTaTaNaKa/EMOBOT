cf3825869a3649e79565cfbd0b1670b4265daebf
yu_to <yuto.aka1127@gmail.com>
2024-10-17 12:34:11 +0900

    メモ追記

--- Programs/src/Main.py
+++ Programs/src/Main.py
@@ -1,11 +1,14 @@
 # モジュール・プログラムファイルのインポート
 import threading
 import time
-import InputVoice
 
 # 処理の記述
+# プログラムの起動・終了、スレッドの管理・作成・実行
 def main():
     print("メイン関数の実行")
+    # スレッドの作成
+    
+    # スレッドの開始
 
 if __name__ == "__main__":
     main()
