f97a97116b70357b2804b7b1343db4481a5b3d28
Suzuki-Eri-code <f23aa016@chuo.ac.jp>
2024-10-25 10:16:39 +0900

    より簡略化した処理に修正

--- Test/suzuki_test/light_senti_analysis.py
+++ Test/suzuki_test/light_senti_analysis.py
@@ -1,12 +1,13 @@
 from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassification
+import torch
 
 # 事前学習済みの日本語感情分析モデルとそのトークナイザをロード
 model_name  = 'lxyuan/distilbert-base-multilingual-cased-sentiments-student'  # 一番使い勝手の良かった軽量モデルに置き換え済。より軽いモデルがあれば、それに置き換え
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForSequenceClassification.from_pretrained(model_name)
+model = AutoModelForSequenceClassification.from_pretrained(model_name,torch_dtype=torch.float16)  # 低精度(float16)でモデルを動作させて計算量を削減
 
 # 感情分析パイプラインの設定
-nlp = pipeline('sentiment-analysis', model=model, tokenizer=tokenizer, truncation=True)
+nlp = pipeline('sentiment-analysis', model=model, tokenizer=tokenizer, truncation=True, max_length=128)  # 最大文字数を128文字に設定する
 
 # 分析対象のテキストリスト
 texts = ['給料が高くて満足しています。', '給料低すぎるだろ！', '可もなく不可もなく']
