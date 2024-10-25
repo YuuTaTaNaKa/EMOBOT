4b40b12c43abca958917d75c6d056458e7e834ec
ryuki0616 <168512845+ryuki0616@users.noreply.github.com>
2024-10-25 12:06:49 +0900

    ifdhispray_main.py

--- Test/sato_test/ifdhispray_main.py
+++ Test/sato_test/ifdhispray_main.py
@@ -2,17 +2,17 @@ import cv2
 from light_senti_analysis.py import emotion
 
 if (emotion == "positive"):
-    image = cv2.imread('Test/sato_test/fubuki.jpg')
+    image = cv2.imread('YuuTaTaNaKa/EMOBOT/Test/sato_test/egaoFBK.png')
     cv2.imshow('Displayed Image', image)
     cv2.waitKey(0) 
     cv2.destroyAllWindows()
-elif(emotion == "positive"):
-    image = cv2.imread('Test/sato_test/fubuki.jpg')
+elif(emotion == "negative"):
+    image = cv2.imread('YuuTaTaNaKa/EMOBOT/Test/sato_test/kanasimiFBK.png')
     cv2.imshow('Displayed Image', image)
     cv2.waitKey(0) 
     cv2.destroyAllWindows()
 else:
-    image = cv2.imread('Test/sato_test/fubuki.jpg')
+    image = cv2.imread('YuuTaTaNaKa/EMOBOT/Test/sato_test/fubuki.jpg')
     cv2.imshow('Displayed Image', image)
     cv2.waitKey(0) 
     cv2.destroyAllWindows()
