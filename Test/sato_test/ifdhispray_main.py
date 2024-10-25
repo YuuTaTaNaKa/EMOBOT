033819ad817ad9358f81b9c48bb196c30602f48b
ryuki0616 <168512845+ryuki0616@users.noreply.github.com>
2024-10-25 12:17:44 +0900

    ifdhispray_main.py

--- Test/sato_test/ifdhispray_main.py
+++ Test/sato_test/ifdhispray_main.py
@@ -1,18 +1,20 @@
 import cv2
-from light_senti_analysis.py import emotion
 
-if (emotion == "positive"):
-    image = cv2.imread('YuuTaTaNaKa/EMOBOT/Test/sato_test/egaoFBK.png')
-    cv2.imshow('Displayed Image', image)
-    cv2.waitKey(0) 
-    cv2.destroyAllWindows()
-elif(emotion == "negative"):
-    image = cv2.imread('YuuTaTaNaKa/EMOBOT/Test/sato_test/kanasimiFBK.png')
-    cv2.imshow('Displayed Image', image)
-    cv2.waitKey(0) 
-    cv2.destroyAllWindows()
-else:
-    image = cv2.imread('YuuTaTaNaKa/EMOBOT/Test/sato_test/fubuki.jpg')
-    cv2.imshow('Displayed Image', image)
-    cv2.waitKey(0) 
-    cv2.destroyAllWindows()
+def display_emotion_image(emotion):
+    if emotion == "positive":
+        image_path = 'YuuTaTaNaKa/EMOBOT/Test/sato_test/egaoFBK.png'
+    elif emotion == "negative":
+        image_path = 'YuuTaTaNaKa/EMOBOT/Test/sato_test/kanasimiFBK.png'
+    else:
+        image_path = 'YuuTaTaNaKa/EMOBOT/Test/sato_test/fubuki.jpg'
+    
+    # 画像を読み込む
+    image = cv2.imread(image_path)
+    
+    if image is not None:
+        # 画像を表示する
+        cv2.imshow('Displayed Image', image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    else:
+        print(f"Error: Could not open or find the image at path {image_path}")
