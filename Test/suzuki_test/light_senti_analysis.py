0611829158f4298884b3f4c009f547ab5f4bb177
Suzuki-Eri-code <f23aa016@chuo.ac.jp>
2024-10-29 09:26:31 +0900

    より軽量化したコードに修正
    分析対象のテキストを1つのみに絞ることで軽量化

--- Test/suzuki_test/light_senti_analysis.py
+++ Test/suzuki_test/light_senti_analysis.py
@@ -7,20 +7,19 @@ tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForSequenceClassification.from_pretrained(model_name,torch_dtype=torch.float16)  # 低精度(float16)でモデルを動作させて計算量を削減
 
 # 感情分析パイプラインの設定
-nlp = pipeline('sentiment-analysis', model=model, tokenizer=tokenizer, truncation=True, max_length=128)  # 最大文字数を128文字に設定する
+nlp = pipeline('sentiment-analysis', model=model, tokenizer=tokenizer, truncation=True, max_length=512)  # 最大文字数の限界は512文字っぽい？
 
-# 分析対象のテキストリスト
-texts = ['給料が高くて満足しています。', '給料低すぎるだろ！', '可もなく不可もなく']
+# 分析対象のテキスト
+text = "給料が高くて満足しています。"
 
-# 一度にすべてのテキストを処理して結果を取得
-results = nlp(texts)
+# テキストを処理して結果を取得
+result = nlp(text)
 
 # 結果を表示
-for text, result in zip(texts, results):
-    print('*' * 50)
-    print(f'テキスト：{text}')
-    print(f'感情：{result["label"]}')  # positive,negative,neutralの3種類
-    print(f'感情スコア：{result["score"]:.4f}')
+print('*' * 50)
+print(f'テキスト：{text}')
+print(f'感情：{result[0]["label"]}')  # positive,negative,neutralの3種類
+print(f'感情スコア：{result[0]["score"]:.4f}')
 
 # """pip install Cython"""
 # """pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/armv7l transformers"""
