d292591a4df3f98c13aa6d54307e864c333dcab4
shouma0403 <f23aa008@chuo.ac.jp>
2024-11-29 09:58:48 +0900

    a

--- Programs/src/LED.py
+++ Programs/src/LED.py
@@ -2,9 +2,22 @@
 # import gpiozero as LED
 import time
 
-# LEDのインスタンスを生成
-led = 11
-bule = 13
-green = 15
+# 通常時（寝てるとき）
+def led_sleep():
+    print("青色に光る")
 
-# led = LED.LED(led)
+# コマンドを受け付けるとき
+def led_accept():
+    print("緑色に光る")
+
+# コマンドを実行しているとき
+def led_execution():
+    print("黄色に光る")
+
+# エラーが発生したとき
+def led_error():
+    print("赤色に光る")
+
+#音楽を再生しているとき
+def led_music():
+    print("紫色に光る")
