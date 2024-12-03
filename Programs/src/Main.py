a0d91a29728890fcf2c7c086babdad4cfc47a9e6
YuuTaTaNaKa <f23ba051>
2024-12-03 14:40:28 +0900

    a

--- Programs/src/Main.py
+++ Programs/src/Main.py
@@ -202,31 +202,3 @@ if __name__ == "__main__":
     frames = {}
 
     main()
-
-    # # 標準エラー出力をリダイレクト
-    # redirect_stderr_to_logfile()
-
-    # print("スレッドを開始します。")
-
-    # # 各機能に対してデーモンスレッドを作成
-    # voice_thread = threading.Thread(target=InVoice.assistant, daemon=True)
-    # # display_thread = threading.Thread(target=Display, daemon=True)
-    # # led_thread = threading.Thread(target=LED.led, daemon=True)
-
-    # # スレッドをリストに追加
-    # # threads.extend([voice_thread, display_thread])  # led_thread])
-    # threads.extend([voice_thread])  # led_thread])
-
-    # # スレッドを開始
-    # for thread in threads:
-    #     thread.start()
-
-    # # プログラムの終了を防ぐために、適宜待機処理を追加
-    # try:
-    #     while True:
-    #         time.sleep(1)  # 1秒待機
-    # except KeyboardInterrupt:
-    #     print("\n停止処理を実行します...")
-    #     stop()  # Ctrl+Cで停止
-
-    # Display()
