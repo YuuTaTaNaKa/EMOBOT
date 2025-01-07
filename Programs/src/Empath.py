077ebcbe58c868dab1841311e247563e96c8eff6
shouma0403 <f23aa008@chuo.ac.jp>
2025-01-07 12:22:47 +0900

    a

--- Programs/src/Empath.py
+++ Programs/src/Empath.py
@@ -21,3 +21,28 @@ def empath(voice):
 # 使用例
 # empath("path_to_your_audio_file.wav")
 
+"""
+エモボットスコア
+
+最大値５０ 最低値０
+
+冷静さ、怒り、悲しみ、喜び、活気
+
+thinEye
+冷静さ２０～５０
+
+anger
+怒り２０～５０
+
+sad
+悲しみ２０～５０
+
+kirarin
+喜び２５～５０
+活気２５～５０
+
+smile
+喜び１５～２５
+活気１５～２５
+
+"""
