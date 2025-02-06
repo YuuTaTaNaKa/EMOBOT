b513bebc519b3166c152533fb02be9be1114a7ca
ryuki0616 <f23ba035@chuo.ac.jp>
2025-02-06 15:40:18 +0900

    test

---
+++ Test/sato_test/mota.py
@@ -0,0 +1,19 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)            # GPIOモード設定
+servo_pin = 17                    # GPIOピン番号
+GPIO.setup(servo_pin, GPIO.OUT)   # GPIO出力モード
+pwm = GPIO.PWM(servo_pin, 50)     # 周波数50Hz
+pwm.start(1.45)                   # パルス幅1.45ms＝ニュートラルポジション
+
+try:
+    while True:
+        pwm.ChangeDutyCycle(12)   # パルス幅12msで、角度は約180度
+        time.sleep(2)
+        pwm.ChangeDutyCycle(2.5)  # パルス幅2.5msで、角度は約0度
+        time.sleep(2)
+
+except KeyboardInterrupt:
+    # Ctrl+Cで終了（PWMを停止）
+    pwm.stop()
