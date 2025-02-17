56b05e0fa599d30f65a83c3d6e3a99fa7943223d
f23ba097 <f23ba097@chuo.ac.jp>
2025-02-17 18:10:33 +0900

    a

--- Test/sato_test/pin_test_out.py
+++ Test/sato_test/pin_test_out.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)  # ピン番号の指定方法（BCM or BOARD）
-GPIO.setup(23, GPIO.OUT,pull_up_down=GPIO.PUD_DOWN)  # ピン 23 を出力モードに設定
+GPIO.setup(23, GPIO.OUT)  # ピン 23 を出力モードに設定
 
 GPIO.output(23, GPIO.HIGH)  # ピンを HIGH にする
 time.sleep(2)  # 2秒待機
