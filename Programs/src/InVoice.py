bf4c350cce58044224b8aa5bcefe4b2bad49cd6d
f23ba097 <f23ba097@chuo.ac.jp>
2025-02-18 12:15:18 +0900

    abcde

--- Programs/src/InVoice.py
+++ Programs/src/InVoice.py
@@ -51,8 +51,8 @@ def listen(mic_timeout, phrase_time_limit,number):
                 converted_audio_file = "converted_audio.wav"
                 convert_sample_rate(temp_audio_file, converted_audio_file)
                 #ファイルを5秒にトリミング
-                print("トリミング直前")
-                trim_audio(converted_audio_file, max_duration=5)
+                print("トリミング中止")
+                # trim_audio(converted_audio_file, max_duration=5)
 
                 # 音声認識を実行
                 command = recognizer.recognize_google(audio, language='ja-JP')  # 日本語設定               
