baa9327051046711eb9522f2371307313f7bf105
ryuki0616 <f23ba035@chuo.ac.jp>
2025-02-19 19:28:53 +0900

    sound

--- Test/sato_test/outsound_test.py
+++ Test/sato_test/outsound_test.py
@@ -2,12 +2,12 @@ import pygame
 
 # ① pygameのミキサーを初期化
 pygame.mixer.init()
-
+print("音声を再生します")
 # ② 音声ファイルをロード
 pygame.mixer.music.load("kirarin.mp3")  # または .wav / .ogg など
-
+print("音声を再生します")
 # ③ 再生
-pygame.mixer.music.play()
+pygame.mixer.music.play(1)
 
 # ④ 再生が終わるまで待つ（省略可能）
 while pygame.mixer.music.get_busy():
