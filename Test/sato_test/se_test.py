c8926c902f6ae63ab707ef95c45461a975280bad
ryuki0616 <f23ba035@chuo.ac.jp>
2025-02-20 14:50:43 +0900

    wav

--- Test/sato_test/se_test.py
+++ Test/sato_test/se_test.py
@@ -3,7 +3,7 @@ import pygame
 pygame.mixer.init()  # 初期化
 
 # 効果音をロード
-se = pygame.mixer.Sound("/home/emobot2/Desktop/EMOBOT/Test/sato_test/kirarin_Pitch Changer.wav")
+se = pygame.mixer.Sound("/home/emobot2/Desktop/EMOBOT/Test/sato_test/kirarin_PitchChanger.wav")
 
 # 効果音を再生
 se.play()
